c3273b0007074dcaf4c1ec2919d9811bdfb40f3f
Ivan Karl <ivan6258@gmail.com>
2013-11-10 18:46:04 -0300

    first app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agalma::Application.config.secret_key_base = 'ab4796bf84b2aff039d93f2133bdf1e3deea8000b247e8427722048d20aeb22588272cd5fd753d7d97e57ccc38d473ba30b041e31f58d34808caaa94d8cfa8a4'
