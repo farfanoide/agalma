07b01264deddd30a4d66e98d9931e6f706477b61
Ivan Karl <ivan6258@gmail.com>
2013-11-11 02:11:54 -0300

    removes invalid parameter, updates views

--- app/models/rolification.rb
+++ app/models/rolification.rb
@@ -2,4 +2,8 @@ class Rolification < ActiveRecord::Base
   belongs_to :branch
   belongs_to :role
   belongs_to :user
+
+  validates_associated :role
+  validates_associated :user
+  validates_associated :branch
 end
