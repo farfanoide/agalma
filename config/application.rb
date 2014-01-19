49644d9ae6445e9bc33d0ab94e1c0521695e5f75
Ivan Karl <ivan6258@gmail.com>
2014-01-19 18:57:13 -0300

    ckeditor test for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Agalma
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # ckeditor stuff for production
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
   end
 end
