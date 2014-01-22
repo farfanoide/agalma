11d77cffe40df3c0cd298ea9ea08c96ff5516edb
Ivan Karl <ivan6258@gmail.com>
2014-01-22 08:16:32 -0300

    adds seeds for some pages, implements navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :fetch_static_pages
+
+  private
+    def fetch_static_pages
+        @quienes = Page.find_by!(slug: 'quienes-somos')
+        @cTeraEd = Page.find_by!(slug: 'centros-terapeuticos-educativos')
+        @cEsTemp = Page.find_by!(slug: 'centro-de-estimulacion-temprana')
+        # @quienes = Page.find_by!(slug: 'quienes-somos')
+    end
 end
