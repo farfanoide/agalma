986412992c8067e7379f9bbbb763daed22b15576
chudix <stkboy@gmail.com>
2014-01-24 16:53:50 -0300

    some new structure styling

--- db/schema.rb
+++ db/schema.rb
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 20140122182229) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
-  create_table "images", force: true do |t|
-    t.string   "name"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "images", ["post_id"], name: "index_images_on_post_id", using: :btree
-
   create_table "pages", force: true do |t|
     t.string   "title"
     t.string   "slug"
@@ -57,26 +48,6 @@ ActiveRecord::Schema.define(version: 20140122182229) do
   end
 
   add_index "pages", ["slug"], name: "index_pages_on_slug", using: :btree
-
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rich_rich_files", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "rich_file_file_name"
-    t.string   "rich_file_content_type"
-    t.integer  "rich_file_file_size"
-    t.datetime "rich_file_updated_at"
-    t.string   "owner_type"
-    t.integer  "owner_id"
-    t.text     "uri_cache"
-    t.string   "simplified_type",        default: "file"
-  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
