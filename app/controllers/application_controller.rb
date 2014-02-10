1728b320c89903bfa2ed6dbbc79bcf91669d430c
chudix <stkboy@gmail.com>
2014-02-10 00:11:57 -0300

    foundation buttons off + some sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :fetch_static_pages
+  before_filter :fetch_static_pages, :set_menu_pages
 
   private
     def fetch_static_pages
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
         @cTeraEd ||= Page.find_by!(slug: 'centros-terapeuticos-educativos')
         @cEsTemp ||= Page.find_by!(slug: 'centro-de-estimulacion-temprana')
         # @quienes ||= Page.find_by!(slug: 'quienes-somos')
+    end
+    
+    def set_menu_pages
+      @pages = Page.all
     end
 end
