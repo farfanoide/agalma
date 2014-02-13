2d7012557afc2c0aa09616a627dc4da94cf611c2
nestier <fernan.nestier@gmail.com>
2014-02-13 15:22:31 -0300

    add strong parameters device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :fetch_static_pages
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
 
@@ -25,5 +26,11 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
       flash[:error] = "You are not authorized to perform this action."
       redirect_to request.headers["Referer"] || root_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
     end
 end
