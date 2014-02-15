2fc3bdea808041a17e1898a47ef29e2aaeff38c7
chudix <stkboy@gmail.com>
2014-02-15 14:07:02 -0300

    sends contact us mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,14 @@ Agalma::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   '587',
+    user_name:              'ilstucardo',
+    password:               'ninjakick86',
+    autentication:          'plain',
+    enable_starttls_auto:   true,
+  }
 end
 
 silence_warnings do
