1ddc71a0a3a0968f231551c938d9b52c68f914c1
Ivan Karl <ivan6258@gmail.com>
2014-02-18 18:31:09 -0300

    format that shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,29 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_filter :fetch_static_pages
-  include Pundit
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    before_filter :fetch_static_pages
+    include Pundit
 
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
 
-  private
-    def fetch_static_pages
-        @quienes ||= Page.find_by!(slug: 'quienes-somos')
-        @cTeraEd ||= Page.find_by!(slug: 'centros-terapeuticos-educativos')
-        @cEsTemp ||= Page.find_by!(slug: 'centro-de-estimulacion-temprana')
-        # @quienes ||= Page.find_by!(slug: 'quienes-somos')
-    end
+    private
+        def fetch_static_pages
+            @quienes ||= Page.find_by!(slug: 'quienes-somos')
+            @cTeraEd ||= Page.find_by!(slug: 'centros-terapeuticos-educativos')
+            @cEsTemp ||= Page.find_by!(slug: 'centro-de-estimulacion-temprana')
+            # @quienes ||= Page.find_by!(slug: 'quienes-somos')
+        end
 
-    def set_menu_pages
-      @pages = Page.all
-    end
+        def set_menu_pages
+            @pages = Page.all
+        end
 
 
-    def user_not_authorized
-      flash[:error] = "You are not authorized to perform this action."
-      redirect_to request.headers["Referer"] || root_path
-    end
+        def user_not_authorized
+            flash[:error] = "You are not authorized to perform this action."
+            redirect_to request.headers["Referer"] || root_path
+        end
 end
--- app/controllers/backend/dashboard_controller.rb
+++ app/controllers/backend/dashboard_controller.rb
@@ -1,10 +1,8 @@
 class Backend::DashboardController < BackendController
 
-
-  
-  def home
-    @pages = Page.all
-    @branches = Branch.all
-    @roles = Rolification.all.group :id, :user_id
-  end
+    def home
+        @pages = Page.all
+        @branches = Branch.all
+        @roles = Rolification.all.group :id, :user_id
+    end
 end
