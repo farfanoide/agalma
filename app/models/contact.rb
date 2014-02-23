0b275d92bd6ae938dd38b3c539aeb35e4b985afd
nestier <fernan.nestier@gmail.com>
2014-02-23 19:32:49 -0300

    mailer backend a varios usuarios como el orto

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < MailForm::Base
   attribute :name,    validate:  true
   attribute :email,   validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
+  attribute :to
 
-  validates_presence_of :message
+  validates_presence_of :message,:to,:email
 end
