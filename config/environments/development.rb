d41d3f0b1570eeff0590925aade7940cad006b00
Ivan Karl <ivan6258@gmail.com>
2014-03-15 18:26:11 -0300

    adds sourcemaps for sass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Agalma::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-# In the development environment your application's code is reloaded on
+  # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
@@ -38,6 +38,18 @@ Agalma::Application.configure do
     enable_starttls_auto:   true,
   }
 
+  #sass debugging
+
+  # Expands the lines which load the assets
+  # config.assets.debug = true // ya esta puesto mas arriba
+  # Enable source maps in the browser
+  config.sass.debug_info = true
+  # source maps don't get output if this is true
+  config.sass.line_comments = false
+  # Do not compress assets
+  config.assets.compress = false
+  # config.assets.compile = true
+  # config.serve_static_assets = false
 end
 
 silence_warnings do
