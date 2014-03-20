03b2e207c0c78b7e132958fc511c462d589928f7
Ivan Karl <ivan6258@gmail.com>
2014-03-20 10:26:18 -0300

    fixes sign_out error backend
    removes various unused js plugins from backend
     some aesthetic changes in backend forms, header and sidebar

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,14 +1,15 @@
 //= require jquery
+//= require jquery_ujs
 //= require ckeditor/override
 //= require ckeditor/init
 //= require bootstrap.min.js
-//= require plugins/morris/morris.min.js
-//= require plugins/sparkline/jquery.sparkline.min.js
-//= require plugins/jvectormap/jquery-jvectormap-1.2.2.min.js
-//= require plugins/jvectormap/jquery-jvectormap-world-mill-en.js
-//= require plugins/fullcalendar/fullcalendar.min.js
-//= require plugins/jqueryKnob/jquery.knob.js
+// plugins/morris/morris.min.js
+// plugins/sparkline/jquery.sparkline.min.js
+// plugins/jvectormap/jquery-jvectormap-1.2.2.min.js
+// plugins/jvectormap/jquery-jvectormap-world-mill-en.js
+// plugins/fullcalendar/fullcalendar.min.js
+// plugins/jqueryKnob/jquery.knob.js
 //= require plugins/daterangepicker/daterangepicker.js
-//= require plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js
+// plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js
 //= require plugins/iCheck/icheck.min.js
 //= require adminapp.js
