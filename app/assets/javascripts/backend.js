4e5e2dfe272f54dd089933c97a81684f990d9d2e
Ivan Karl <ivan6258@gmail.com>
2014-03-19 23:50:41 -0300

    modularizes backend header

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -12,32 +12,3 @@
 //= require plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js
 //= require plugins/iCheck/icheck.min.js
 //= require adminapp.js
-
-        // <!-- jQuery 2.0.2 -->
-        // <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        // <!-- jQuery UI 1.10.3 -->
-        // <script src="js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
-        // <!-- Bootstrap -->
-        // <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        // <!-- Morris.js charts -->
-//         <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-//         <script src="plugins/morris/morris.min.js" type="text/javascript"></script>
-//         <!-- Sparkline -->
-//         <script src="plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
-//         <!-- jvectormap -->
-//         <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>
-//         <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
-//         <!-- fullCalendar -->
-//         <script src="plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
-//         <!-- jQuery Knob Chart -->
-//         <script src="plugins/jqueryKnob/jquery.knob.js" type="text/javascript"></script>
-//         <!-- daterangepicker -->
-//         <script src="plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>
-//         <!-- Bootstrap WYSIHTML5 -->
-//         <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
-//         <!-- iCheck -->
-//         <script src="plugins/iCheck/icheck.min.js" type="text/javascript"></script>
-//
-//         <!-- AdminLTE App -->
-//         <script src="js/AdminLTE/app.js" type="text/javascript"></script>
-//
