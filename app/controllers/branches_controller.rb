7d7907ccc496fdc5038c653fb11fac27f741762f
Ivan Karl <ivan6258@gmail.com>
2014-03-23 01:46:07 -0300

    reindents controllers to use 2 spaces

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -1,25 +1,25 @@
 class BranchesController < ApplicationController
 
-    before_action :set_branch, only: [:show]
+  before_action :set_branch, only: [:show]
 
-    # GET /branches
-    # GET /branches.json
-    def index
-        @branches = Branch.all
+  # GET /branches
+  # GET /branches.json
+  def index
+    @branches = Branch.all
+  end
+
+  # # GET /branches/1
+  # # GET /branches/1.json
+  def show
+  end
+
+  def consultorios_externos
+    @branches = Branch.all
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_branch
+      @branch = Branch.find(params[:id])
     end
-
-    # # GET /branches/1
-    # # GET /branches/1.json
-    def show
-    end
-
-    def consultorios_externos
-        @branches = Branch.all
-    end
-
-    private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_branch
-            @branch = Branch.find(params[:id])
-        end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,19 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show]
 
-    def index
-        @posts = Post.order('created_at DESC').paginate(page: params[:page], per_page: 5)
+  def index
+    @posts = Post.order('created_at DESC').paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
     end
 
-    def show
+    def post_params
+      params.require(:post).permit(:id )
     end
-
-    private
-
-        def set_post
-            @post = Post.find(params[:id])
-        end
-
-        def post_params
-            params.require(:post).permit(:id )
-        end
 end
