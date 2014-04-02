5495510105eb9e87f43c86d980895ba1598e86c1
Ivan Karl <ivan6258@gmail.com>
2014-04-02 09:53:21 -0300

    fixes assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Agalma::Application.configure do
   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w(ckeditor/*)
+  config.assets.precompile += %w(backend.js backend.css)
 end
